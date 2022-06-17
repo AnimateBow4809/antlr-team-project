109cf8400245743f15eb52e26058c6da64e0732e
AliDashtBozorg <alidashtbozorg709@gmail.com>
2022-06-17 16:55:58 +0430

    finished

--- discrete-mathematics-project/src/main/java/ir/ac/kntu/Main.java
+++ discrete-mathematics-project/src/main/java/ir/ac/kntu/Main.java
@@ -1,7 +1,30 @@
 package ir.ac.kntu;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+import java.util.concurrent.ThreadPoolExecutor;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws StackOverflowError {
+        Scanner scanner = new Scanner(System.in);
+        int points = scanner.nextInt();
+        int lines = scanner.nextInt();
+        Geraph geraph = new Geraph(points, lines);
+        geraph.setList(scanner);
+        System.out.println(Arrays.deepToString(geraph.getList()));
+        ArrayList<Engine> engines = new ArrayList<>();
+
+        for (int i = 0; i < points; i++) {
+            engines.add(new Engine(geraph, i + 1));
+            engines.get(i).run();
+        }
+//        System.out.println(engines.get(0).getMin() + "mmqqm");
+        engines.sort(Comparator.comparing(Engine::getMin));
+        for (int i = 0; i < points; i++) {
+            System.out.println(engines.get(i).getMin()+" "+engines.get(i).getPoint());
+        }
 
     }
 }
